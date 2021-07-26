4d09b7cd027c791ebcceab8593dddf3ec36c3282
ishuks9 <ishuks9@gmail.com>
2021-07-26 16:53:13 +0530

    updated the UC9

--- RegexUser.java
+++ RegexUser.java
@@ -3,34 +3,56 @@ import java.util.regex.Pattern;
 
 public class RegexUser {
 	public static void main(String[] args) {
+	String regex_for_Email = "[0-9a-zA-Z]+([+._-]{1}[0-9a-zA-Z]+)?@[0-9a-zA-Z]+[.]{1}[a-zA-z]{2,4}([.]{1}[a-zA-z]{2,3})?$"
+				+ "";
 
-		String regex_for_name = "^[a-zA-Z]{0,6}";
-		String regex_for_LastName = "^[a-zA-Z]{0,10}";
-		String regex_for_Email = "^[a-zA-Z0-9+.-]+@[a-zA-Z0-9.]+$";
-		String regex_for_mobileNumber = "^[0-9+ ]{0,3}[0-9]{0,10}";
-		String regex_for_Password = "(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$";
+		Pattern pattern = Pattern.compile(regex_for_Email);
 
-		Pattern pattern = Pattern.compile(regex_for_name);
-		Matcher matcher = pattern.matcher("Ishu");
+		Matcher matcher1 = pattern.matcher("abc@yahoo.com");
+		Matcher matcher2 = pattern.matcher("abc.100@yahoo.com");
+		Matcher matcher3 = pattern.matcher("abc111@abc.com");
+		Matcher matcher4 = pattern.matcher("abc-100@abc.net");
+		Matcher matcher5 = pattern.matcher("abc.100@abc.com.au");
+		Matcher matcher6 = pattern.matcher("abc-100@yjkjahoo.om");
+		Matcher matcher7 = pattern.matcher("abc@1.com");
+		Matcher matcher8 = pattern.matcher("abc@gmail.com.com");
+		Matcher matcher9 = pattern.matcher("abc+100@gmail.com");
+		Matcher matcher10 = pattern.matcher("abc");
+		Matcher matcher11 = pattern.matcher("abc@.com.my");
+		Matcher matcher12 = pattern.matcher("abc123@gmail.a");
+		Matcher matcher13 = pattern.matcher("abc123@.com");
+		Matcher matcher14 = pattern.matcher("abc123@.com.com");
+		Matcher matcher15 = pattern.matcher(".abc@abc.com");
+		Matcher matcher16 = pattern.matcher("abc()*@gmail.com");
+		Matcher matcher17 = pattern.matcher("abc@%*.com");
+		Matcher matcher18 = pattern.matcher("abc..2002@gmail.com");
+		Matcher matcher19 = pattern.matcher("abc.@gmail.com");
+		Matcher matcher20 = pattern.matcher("abc@abc@gmail.com");
+		Matcher matcher21 = pattern.matcher("abc@gmail.com.1a");
+		Matcher matcher22 = pattern.matcher("abc@gmail.com.aa.au");
 
-		Pattern pattern1 = Pattern.compile(regex_for_LastName);
-		Matcher matcher1 = pattern1.matcher("Singh");
-
-		Pattern pattern2 = Pattern.compile(regex_for_Email);
-		Matcher matcher2 = pattern2.matcher("ishuks9@gmail.com");
-
-		Pattern pattern3 = Pattern.compile(regex_for_mobileNumber);
-		Matcher matcher3 = pattern3.matcher("91 9346779142");
-
-		Pattern pattern4 = Pattern.compile(regex_for_Password);
-		Matcher matcher4 = pattern4.matcher("ishu#I4");
-
-		System.out.println("FirstName Valid : " + matcher.matches());
-		System.out.println("LastName Valid : " + matcher1.matches());
-
-		System.out.println("Email Valid : " + matcher2.matches());
-		System.out.println("Mobile number is Valid : " + matcher3.matches());
-		System.out.println("Password Valid : " + matcher4.matches());
+		System.out.println("Email 1 Valid : " + matcher1.matches());
+		System.out.println("Email 2 Valid : " + matcher2.matches());
+		System.out.println("Email 3 Valid : " + matcher3.matches());
+		System.out.println("Email 4 Valid : " + matcher4.matches());
+		System.out.println("Email 5 Valid : " + matcher5.matches());
+		System.out.println("Email 6 Valid : " + matcher6.matches());
+		System.out.println("Email 7 Valid : " + matcher7.matches());
+		System.out.println("Email 8 Valid : " + matcher8.matches());
+		System.out.println("Email 9 Valid : " + matcher9.matches());
+		System.out.println("Email 10 Valid : " + matcher10.matches());
+		System.out.println("Email 11 Valid : " + matcher11.matches());
+		System.out.println("Email 12 Valid : " + matcher12.matches());
+		System.out.println("Email 13 Valid : " + matcher13.matches());
+		System.out.println("Email 14 Valid : " + matcher14.matches());
+		System.out.println("Email 15 Valid : " + matcher15.matches());
+		System.out.println("Email 16 Valid : " + matcher16.matches());
+		System.out.println("Email 17 Valid : " + matcher17.matches());
+		System.out.println("Email 18 Valid : " + matcher18.matches());
+		System.out.println("Email 19 Valid : " + matcher19.matches());
+		System.out.println("Email 20 Valid : " + matcher20.matches());
+		System.out.println("Email 21 Valid : " + matcher21.matches());
+		System.out.println("Email 22 Valid : " + matcher22.matches());
 
 	}
 
