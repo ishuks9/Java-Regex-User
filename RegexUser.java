ebd8ba8425155063e29a52363c7feecf55ea8cd2
ishuks9 <ishuks9@gmail.com>
2021-07-26 16:25:01 +0530

    updated the UC5

--- RegexUser.java
+++ RegexUser.java
@@ -9,6 +9,7 @@ public class RegexUser {
 		String regex_for_LastName =  "^[a-zA-Z]{0,10}";
 		String regex_for_Email = "^[a-zA-Z0-9+.-]+@[a-zA-Z0-9.]+$" ;
 		String regex_for_mobileNumber = "^[0-9+ ]{0,3}[0-9]{0,10}";
+		String regex_for_Password = "[A-Za-z0-9@_#$%!]{8,}";
 		
 		Pattern pattern = Pattern.compile(regex_for_name);
 		Matcher matcher = pattern.matcher("Ishu");
@@ -22,14 +23,16 @@ public class RegexUser {
 		Pattern pattern3 = Pattern.compile(regex_for_mobileNumber);
 		Matcher matcher3 = pattern3.matcher("91 9346779142");
 		
+		Pattern pattern4 = Pattern.compile(regex_for_Password);
+		Matcher matcher4 = pattern4.matcher("IAHyew#@94");
+		
 		System.out.println("FirstName Valid : " +matcher.matches());
 		System.out.println("LastName Valid : " +matcher1.matches());
 		System.out.println("Email Valid : " +matcher2.matches());
 		System.out.println("Mobile number is Valid : "+matcher3.matches());
+		System.out.println("Password Valid : " +matcher4.matches());
 		
 		
 		
 		}
-	
-
-}
+	}
