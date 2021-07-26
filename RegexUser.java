5440a42227e1d72dad45352e8e3dd0201101305f
ishuks9 <ishuks9@gmail.com>
2021-07-26 16:10:41 +0530

    updated the UC2

--- RegexUser.java
+++ RegexUser.java
@@ -6,14 +6,17 @@ public class RegexUser {
 		
 		
 		String regex_for_name = "^[a-zA-Z]{0,6}";
+		String regex_for_LastName =  "^[a-zA-Z]{0,10}";
 		
 		Pattern pattern = Pattern.compile(regex_for_name);
 		Matcher matcher = pattern.matcher("Ishu");
 		
+		Pattern pattern1 = Pattern.compile(regex_for_LastName);
+		Matcher matcher1 = pattern1.matcher("Singh");
+		
 		System.out.println("FirstName Valid : " +matcher.matches());
+		System.out.println("LastName Valid : " +matcher1.matches());
 		
 		
 		}
-	
-
-}
+	}
